06ee9c30c79e3eb993db8cc51b095f49d7336854
Drew Daniels <drew@thedaniels.com>
2022-04-07 14:55:53 -0500

    Added placeholder values for project tech stack items and descriptions and resized project gifs.

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -29,13 +29,70 @@ function Projects(props) {
             'Memory Card', 
             MemoryCardGif,
             ['react', 'javascript', 'html'],
-            'Memory Card game created with React'),
-        new Project('Battleship', BattleshipGif),
-        new Project('Etch-a-Sketch', EtchASketchGif),
-        new Project('Tic Tac Toe', TicTacToeGif),
-        new Project('Todo App', TodoTitanGif),
-        new Project('Weather App', WeatherAppGif),
-        new Project('CV Application', CVProjectGif)
+            'Memory Card game created with React'
+        ),
+        new Project(
+            'Battleship',
+            BattleshipGif,
+            [
+                'jest',
+                'webpack',
+            ],
+            'Battleship game created to utilize test-driven development'
+        ),
+        new Project(
+            'Etch-a-Sketch',
+            EtchASketchGif,
+            [
+                'html',
+                'css',
+                'javascript',
+            ],
+            'Etch-a-Sketch description here'
+        ),
+        new Project(
+            'Tic Tac Toe',
+            TicTacToeGif,
+            [
+                'html',
+                'css',
+                'javascript',
+            ],
+            'Tic Tac Toe description here'
+        ),
+        new Project(
+            'Todo App',
+            TodoTitanGif,
+            [
+                'webpack',
+                'eslint',
+                'html',
+                'css',
+                'javascript',
+                'localstorage-api',
+            ],
+            'Todo app description here'
+        ),
+        new Project(
+            'Weather App', 
+            WeatherAppGif,
+            [
+                'webpack',
+                'openweathermap-api',
+            ],
+            'Weather app description here'),
+        new Project(
+            'CV Application',
+            CVProjectGif,
+            [
+                'react',
+                'html',
+                'css',
+                'javascript',
+                'webpack',
+            ],
+            'CV Application description here'
+        )
     ])
 
     return (
@@ -45,7 +102,15 @@ function Projects(props) {
                 {projects.map(function(project, i) {
                     return (
                         <li key={i} className='project'>
-                        <h2>{project.name}</h2>
+                        <div className='project-metadata'>
+                            <h2 className='project-header'>{project.name}</h2>
+                            <p>{project.description}</p>
+                            <ul>{project.techStack.map((stackItem, i) => {
+                                return (
+                                    <li key={i}>{stackItem}</li>
+                                )
+                            })}</ul>
+                        </div>
                         <div className='project-gif-container'>
                             <img src={project.gif} alt={project.name + ' gif'} className='project-gif' />
                         </div>
