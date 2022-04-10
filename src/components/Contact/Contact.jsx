43ae5bcaf5713b4afd26bb1ecbe12129382164ac
Drew Daniels <drew@thedaniels.com>
2022-04-10 12:57:36 -0500

    Improved the form layout.

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,33 +1,41 @@
 import './Contact.css';
-import { Container, Form, Row, Col, Button } from 'react-bootstrap';
-
+import { Container, Card, Form, Row, Col, Button } from 'react-bootstrap';
+import { IoIosSend as SendIcon} from 'react-icons/io';
 
 function Contact() {
 
     return (
         <Container id='contact'>
-            <h2>Contact me</h2>
-            <Form>
-                <Row>
-                    <Col>
+            <Card>
+                <Card.Header as='h2'>Contact Me</Card.Header>
+                <Card.Body>
+                    <Form>
+                        <Row>
+                            <Col>
+                                <Form.Group className='mb-3' controlId='formBasicEmail'>
+                                    <Form.Label visuallyHidden>First Name</Form.Label>
+                                    <Form.Control type='email' placeholder='First name' />
+                                </Form.Group>
+                            </Col>
+                            <Col>
+                                <Form.Group className='mb-3' controlId='formBasicEmail'>
+                                    <Form.Label visuallyHidden>Last Name</Form.Label>
+                                    <Form.Control type='email' placeholder='Last name' />
+                                </Form.Group>                        
+                            </Col>
+                        </Row>
                         <Form.Group className='mb-3' controlId='formBasicEmail'>
-                            <Form.Label visuallyHidden>First Name</Form.Label>
-                            <Form.Control type='email' placeholder='First name' />
+                            <Form.Label visuallyHidden>Message</Form.Label>
+                            <Form.Control as='textarea' rows={3} placeholder='Type message' />
                         </Form.Group>
-                    </Col>
-                    <Col>
-                        <Form.Group className='mb-3' controlId='formBasicEmail'>
-                            <Form.Label visuallyHidden>Last Name</Form.Label>
-                            <Form.Control type='email' placeholder='Last name' />
-                        </Form.Group>                        
-                    </Col>
-                </Row>
-                <Form.Group className='mb-3' controlId='formBasicEmail'>
-                    <Form.Label visuallyHidden>Message</Form.Label>
-                    <Form.Control as='textarea' rows={3} placeholder='Type message' />
-                </Form.Group>
-                <Button type='submit'>Send</Button>
-            </Form>
+                        <Button type='submit'>
+                            <span>Send</span>
+                            <SendIcon style={{marginLeft: '.25em'}}/>
+                        </Button>
+                    </Form>
+                </Card.Body>
+            </Card>
+
         </Container>
         
     )
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,14 +1,9 @@
 import './Footer.css';
 
-import { RiArrowUpSLine as ScrollUpIcon } from 'react-icons/ri';
-
 function Footer(props) {
 
     return (
         <footer className='footer'>
-            <a href="#home">
-                <ScrollUpIcon />
-            </a>
         </footer>
     )
 }
